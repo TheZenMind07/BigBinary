a214d40cd6a149877acf267d9aaa8a5fd8a662af
Rajkishor Pradhan <rajp7598@gmail.com>
2021-04-17 13:34:42 +0530

    Ract Forms

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 
 function App() {
-  const [name, setname] = useState("");
-  function
-  return (
-    <div className="container">
-      <h1>Hello {name} </h1>
-      <input type="text" placeholder="What's your name?" value ={name} />
-      <button onClick={click}>Submit</button>
-    </div>
-  );
+    const [name, setname] = useState("");
+    const [fname, setfname] = useState("");
+    function changeHandler() {
+        setname(event.target.value);
+    }
+
+    function clickHandler() {
+        setfname(name);
+    }
+
+    return (
+        <div className="container">
+            <h1>Hello {fname} </h1>
+            <input onChange={changeHandler} type="text" placeholder="What's your name?" value={name} />
+            <button onClick={clickHandler}>Submit</button>
+        </div>
+    );
 }
 
 export default App;
