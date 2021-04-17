1d77babde86be43b4cbb47d73dec97eb639b18f0
Rajkishor Pradhan <rajp7598@gmail.com>
2021-04-17 17:58:33 +0530

    Complex companent practice done

--- changing-complex-state-practice/src/components/App.jsx
+++ changing-complex-state-practice/src/components/App.jsx
@@ -1,26 +1,57 @@
 import React, { useState } from "react";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
+    const [contact, setContact] = useState({
+        fName: "",
+        lName: "",
+        email: ""
+    });
 
-  return (
-    <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+    function clickHandler(event) {
+        let field = event.target.name;
+        let val = event.target.value;
+
+        if (field === "fName") {
+            setContact(prev => {
+                return {
+                    fName: val,
+                    lName: prev.lName,
+                    email: prev.email
+                };
+            });
+        } else if (field === "lName") {
+            setContact(prev => {
+                return {
+                    fName: prev.fName,
+                    lName: val,
+                    email: prev.email
+                };
+            });
+        } else if (field === "email") {
+            setContact(prev => {
+                return {
+                    fName: prev.fName,
+                    lName: prev.lName,
+                    email: val
+                };
+            });
+        }
+    }
+
+    return (
+        <div className="container">
+            <h1>
+                Hello {contact.fName} {contact.lName}
+            </h1>
+            <p>{contact.email}</p>
+            <form>
+                <input onChange={clickHandler} name="fName" placeholder="First Name" value={contact.fName} />
+                <input onChange={clickHandler} name="lName" placeholder="Last Name" value={contact.lName} />
+                <input onChange={clickHandler} name="email" placeholder="Email" value={contact.email} />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
