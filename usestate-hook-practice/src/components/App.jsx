3b52a183c7a715cc37292e0493da779818360063
Rajkishor Pradhan <rajp7598@gmail.com>
2021-04-17 11:05:08 +0530

    React Hook practice complete

--- usestate-hook-practice/src/components/App.jsx
+++ usestate-hook-practice/src/components/App.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
-    </div>
-  );
+    let time = new Date().toLocaleTimeString();
+    console.log(time);
+    const [currentTime, latestTime] = useState(time);
+    function refresh() {
+        latestTime(new Date().toLocaleTimeString());
+    }
+
+    setInterval(refresh, 1000);
+
+    return (
+        <div className="container">
+            <h2>{time}</h2>
+            <button onClick={refresh}>Get Time</button>
+        </div>
+    );
 }
 
 export default App;
