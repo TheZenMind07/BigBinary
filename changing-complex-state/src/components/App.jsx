3e4340468874ffc31ee344c962ad088991c6a833
Rajkishor Pradhan <rajp7598@gmail.com>
2021-04-17 17:25:12 +0530

    Changing complex states complted

--- changing-complex-state/src/components/App.jsx
+++ changing-complex-state/src/components/App.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+    const [fullName, setFullName] = useState({
+        fname: "",
+        lname: ""
+    });
+
+    function changeHandler(event) {
+        const field = event.target.name;
+        const val = event.target.value;
+        console.log(event.target);
+        if (field === "fName") {
+            setFullName(prev => {
+                return {
+                    fname: val,
+                    lname: prev.lname
+                };
+            });
+        } else if (field === "lName") {
+            setFullName(prev => {
+                return {
+                    fname: prev.fname,
+                    lname: val
+                };
+            });
+        }
+    }
+
+    // function clickHandler() {
+    //     setfname(name);
+
+    //     event.preventDefault();
+    // }
+
+    return (
+        <div className="container">
+            <h1>
+                Hello {fullName.fname} {fullName.lname}
+            </h1>
+            <form>
+                <input onChange={changeHandler} name="fName" placeholder="First Name" value={fullName.name} />
+                <input onChange={changeHandler} name="lName" placeholder="Last Name" value={fullName.lname} />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
