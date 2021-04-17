1562499e8273580c207c1f0ce20e1d8ceafbd62b
Rajkishor Pradhan <rajp7598@gmail.com>
2021-04-17 12:51:28 +0530

    Event Handling completed

--- event-handling-in-react/src/components/App.jsx
+++ event-handling-in-react/src/components/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+    const [color, setcolor] = useState("white");
+
+    function mouseOut() {
+        setcolor("white");
+    }
+    function mouseOver() {
+        setcolor("black");
+    }
+    return (
+        <div className="container">
+            <h1>Hello</h1>
+            <input type="text" placeholder="What's your name?" />
+            <button style={{ backgroundColor: color }} onMouseOut={mouseOut} onMouseOver={mouseOver}>
+                Submit
+            </button>
+        </div>
+    );
 }
 
 export default App;
